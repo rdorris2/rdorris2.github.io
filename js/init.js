2f573bb7b0a264f88f444980ee0157d015760228
Ryan Dorris <dorris.r@gmail.com>
2019-01-29 10:33:45 -0500

    chgs

--- js/init.js
+++ js/init.js
@@ -512,7 +512,7 @@ function arlo_tm_animate_text(){
 	var animateSpan			= jQuery('.arlo_tm_animation_text_word');
 	
 		animateSpan.typed({
-			strings: ["Data Scientist", "Data Analyist", "Freelancer","Programmer"],
+			strings: ["Data Scientist", "Data Analyst", "Freelancer","Programmer"],
 			loop: true,
 			startDelay: 1e3,
 			backDelay: 2e3
